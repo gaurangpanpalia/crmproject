18fc86517196e32a36ec1d419f4e74494e4c4647
gaurangpsigma <gaurang.p@sigmainfo.net>
2023-11-15 14:43:31 +0530

    dashboard

--- tealcrm/settings.py
+++ tealcrm/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'dashboard',
     'userprofile',
+    
 ]
 
 MIDDLEWARE = [
--- userprofile/models.py
+++ userprofile/models.py
@@ -5,3 +5,7 @@ from django.db import models
 
 class Userprofile(models.Model):
     user=models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+    
