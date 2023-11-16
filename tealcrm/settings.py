3cef3de8e61dd3d61cd842d2cc6450b863a73886
gaurangpsigma <gaurang.p@sigmainfo.net>
2023-11-16 10:53:07 +0530

    changed DB to mysql

--- tealcrm/settings.py
+++ tealcrm/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'tealcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
